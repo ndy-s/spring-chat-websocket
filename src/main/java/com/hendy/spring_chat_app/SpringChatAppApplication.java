1b92eca0ca0c125fe282f3a73f7958a474e17fe9
ndys <hendy.sg02@gmail.com>
2025-04-07 19:06:37 +0700

    feat: add dotenv for creds

--- src/main/java/com/hendy/spring_chat_app/SpringChatAppApplication.java
+++ src/main/java/com/hendy/spring_chat_app/SpringChatAppApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringChatAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringChatAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Dotenv dotenv = Dotenv.configure().load();
+        // dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),
+        // entry.getValue()));
+
+        SpringApplication.run(SpringChatAppApplication.class, args);
+    }
 
 }
